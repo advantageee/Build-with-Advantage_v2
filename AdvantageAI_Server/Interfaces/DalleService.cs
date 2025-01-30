b93966ada5f79982b9d685a0b7a6f029b5ea4159
Oren Shapiro <oshapiro@enginess.io>
2025-01-29 22:51:35 -0500

    Refactor Home view model and add IDalleService interface for image generation

---
+++ AdvantageAI_Server/Interfaces/DalleService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using AdvantageAIWeb.Models.Image;
+
+namespace AdvantageAIWeb.Services.Interfaces
+{
+    /// <summary>
+    /// Interface for DALL-E image generation service.
+    /// </summary>
+    public interface IDalleService
+    {
+        Task<ImageResult> GenerateImageAsync(ImagePromptRequest request);
+        Task<string> GenerateImageAsync(string prompt, string size);
+        Task<string> GenerateImageAsync(string prompt, string size, string style);
+    }
+}
